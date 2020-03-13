bf2343a979cf7cca86f6137a3843c304a2fff69b
Jerryjpg400 <carlLiAcount@gmail.com>
2020-03-13 14:22:34 +0000

    add

--- lecture_code/BlankJavaProject/BlankJavaProject/src/BlankClass.java
+++ lecture_code/BlankJavaProject/BlankJavaProject/src/BlankClass.java
@@ -4,6 +4,10 @@ import java.applet.*;
 
 public class BlankClass extends ConsoleProgram{
 	public void run() {
-		
+		int[] num = new int[8];
+		for (int i = 0; i < num.length; i++) {
+			num[i] = 2*i;
+			println(num[i]);
+		}
 	}
 }
