4d713b6e2aa0039c406f66f35386e8b58529b0bd
Jerryjpg400 <carLiAcount@gmail.com>
2020-03-19 11:52:48 +0000

    update

--- lecture_code/BlankJavaProject/BlankJavaProject/src/BankAccount.java
+++ lecture_code/BlankJavaProject/BlankJavaProject/src/BankAccount.java
@@ -15,15 +15,30 @@ public class BankAccount {
 	
 	
 	/*
-	 * 
+	 *  The method returns true. 
+	 *  If the balance cannot afford all of the fees or has no money left, 
+	 *  the balance is left as 0.0 and the method returns false.
+	 *  **********************************************
+	 *  For example 1
+	 *  transactionFee(10.00);
+	 *  Then the account would be deducted $10 + $20 + $30 + $40 
+	 *  for the four transactions, 
+	 *  leaving a final balance of $0.00. The method would return false.
+	 *  **********************************************
+	 *  For example 2
+	 *  transactionFee(10.00);
+	 *  Then the account would be deducted $5 + $10 + $15 + $20 
+	 *  for the four transactions, 
+	 *  leaving a final balance of $90.00. The method would return true. 
+	 *  If a second call were made
 	 */
 	public boolean transactionFee(double amount) {
-		double deducted = 0;
+		double deducted = 0.00;
 		for (int i = 1; i <= 4; i++) {
 			deducted += i * amount;
 		}
-		balance = balance-deducted;
-		if (balance >= 0.0) {
+		withDraw(deducted);
+		if (balance >= 0.00) {
 			return true;
 		} else {
 			balance = 0.00;
