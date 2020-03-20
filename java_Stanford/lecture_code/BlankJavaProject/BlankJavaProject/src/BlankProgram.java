d23a0cfd6bc5f04b38d54774b36ba512e98b620e
Jerryjpg400 <carLiAcount@gmail.com>
2020-03-20 22:19:24 +0000

    update

--- java_Stanford/lecture_code/BlankJavaProject/BlankJavaProject/src/BlankProgram.java
+++ java_Stanford/lecture_code/BlankJavaProject/BlankJavaProject/src/BlankProgram.java
@@ -1,4 +1,24 @@
+import acm.program.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 
-public class BlankProgram {
+import javax.swing.*;
 
+public class BlankProgram extends Program{
+	public void init() {
+		JButton button1 = new JButton("Botton1");
+		add(button1, NORTH);
+		JButton button2 = new JButton("Button2");
+		add(button2, SOUTH);
+		JLabel label = new JLabel("Hello world!");
+		add(label, CENTER);
+		
+		addActionListeners();
+	}
+	
+	public void actionPerformed(ActionEvent event) {
+		if (event.getActionCommand().equals("Calculate tips!")) {
+			String subtotal = subtotalField.getText();
+		}
+	}
 }
